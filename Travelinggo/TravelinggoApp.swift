d4b271a1c87dc30584ddb99f484d6512e2a11d9a
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-06-20 18:59:45 +0700

    Initial Commit

---
+++ Travelinggo/TravelinggoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelinggoApp.swift
+//  Travelinggo
+//
+//  Created by Farhandika Zahrir Mufti guenia on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelinggoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
